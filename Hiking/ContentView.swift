483eefd68f345d9bb88e322e3eb8758e0f8058df
Basia Madej <basia@lunarlogic.io>
2021-02-02 11:50:13 +0100

    Add Hike model

--- Hiking/ContentView.swift
+++ Hiking/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let hikes = Hike.all()
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(self.hikes, id: \.name) { hike in
+            HikeCell(hike: hike)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct HikeCell: View {
+    let hike: Hike
+    var body: some View {
+        HStack {
+            Image(hike.imageURL)
+                .resizable()
+                .frame(width: 100, height: 100)
+                .cornerRadius(10)
+            Text(hike.name)
+        }
     }
 }
